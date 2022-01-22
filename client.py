7d64b46196f3d88ca25a0891558245ce57b36b03
fsx950223 <fsx950223@gmail.com>
2022-01-22 20:58:29 +0800

    format

--- client.py
+++ client.py
@@ -1,15 +1,32 @@
 import requests
+
 proxies = {
-  "http": None,
-  "https": None,
+    "http": None,
+    "https": None,
 }
 
-res = requests.get('http://127.0.0.1:5000/v1/detection/init', proxies=proxies).json()
+res = requests.get('http://127.0.0.1:5000/v1/detection/init',
+                   proxies=proxies).json()
 task_id = res.get('task_id', None)
 print(task_id)
-res = requests.get('http://127.0.0.1:5000/v1/detection/view', proxies=proxies).json()
+res = requests.get('http://127.0.0.1:5000/v1/detection/view',
+                   proxies=proxies).json()
 print(res.get('tasks', None))
-res = requests.post('http://127.0.0.1:5000/v1/detection/train', proxies=proxies, data={'task_id': task_id, 'train_dataset': 'gs://cloud-ml-data/img/openimage/csv/salads_ml_use.csv', 'test_dataset':'gs://cloud-ml-data/img/openimage/csv/salads_ml_use.csv', 'batch_size':8, 'epochs':10})
-if res.status_code!=200:
-  print(res.json().get('msg', None))
+res = requests.post(
+    'http://127.0.0.1:5000/v1/detection/train',
+    proxies=proxies,
+    data={
+        'task_id':
+            task_id,
+        'train_dataset':
+            'gs://cloud-ml-data/img/openimage/csv/salads_ml_use.csv',
+        'test_dataset':
+            'gs://cloud-ml-data/img/openimage/csv/salads_ml_use.csv',
+        'batch_size':
+            8,
+        'epochs':
+            10
+    })
+if res.status_code != 200:
+    print(res.json().get('msg', None))
 # res = requests.post('http://127.0.0.1:5000/v1/detection/predict', proxies=proxies, data={'task_id': task_id, 'url': 'gs://cloud-ml-data/img/openimage/csv/salads_ml_use.csv', 'batch_size':8})
