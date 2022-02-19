e16ac6d87e7d0b953a965b8d5c9a4b04341497dc
beary <bearyme@outlook.com>
2022-02-20 00:45:59 +0800

    feat(ui): Put layouts in a separate directory
    Signed-off-by: beary <bearyme@outlook.com>

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter } from 'react-router-dom'
-import { App } from './App'
+import { App } from './app'
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 )
