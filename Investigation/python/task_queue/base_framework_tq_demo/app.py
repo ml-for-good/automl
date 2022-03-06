3769b461068d01b69c59fe0fa61a6f9757757ddd
h2222 <1311367609@qq.com>
2022-03-06 17:44:42 +0800

    feat: added reload function to config files.

--- Investigation/python/task_queue/base_framework_tq_demo/app.py
+++ Investigation/python/task_queue/base_framework_tq_demo/app.py
@@ -1,9 +1,9 @@
-
-
+import os
 import flask
 import flask_restful
 from flask_cors import CORS
 
+from GLOBAL_KEY import *
 from module_manager import *
 
 app = flask.Flask(__name__)
@@ -16,9 +16,11 @@ handler_lst = [
 ]
 
 for handler, path, conf in handler_lst:
+    LAST_CONFIG_MODIFY_TIME[conf] = os.stat(conf).st_mtime 
     handler.initialize(conf)
     api.add_resource(handler, path)
 
 
 if __name__ == "__main__":
+    schedule.start()
     app.run(host='0.0.0.0', port='8030', debug=True)
