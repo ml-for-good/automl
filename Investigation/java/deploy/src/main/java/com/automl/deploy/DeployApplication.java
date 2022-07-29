7806f70d171e915b95bb6f32f099691321704a18
chenqi <1535985458@qq.com>
2022-07-30 01:11:59 +0800

    feat(deploy): add java deploy demo

---
+++ Investigation/java/deploy/src/main/java/com/automl/deploy/DeployApplication.java
@@ -0,0 +1,13 @@
+package com.automl.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployApplication.class, args);
+	}
+
+}
