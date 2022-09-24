477938053ed58876002cf02ae7d3e75dd43576dc
qingguox <1367379258@qq.com>
2022-09-24 17:24:11 +0800

    [rebase master and test java link mysql success]

--- java/automl/automl-web/src/main/java/com/automl/web/controller/AutoMlTestController.java
+++ java/automl/automl-web/src/main/java/com/automl/web/controller/AutoMlTestController.java
@@ -8,10 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Api("auto ml test")
 @RestController
--- java/automl/automl-web/src/main/java/com/automl/web/controller/DatasetController.java
+++ java/automl/automl-web/src/main/java/com/automl/web/controller/DatasetController.java
@@ -1,14 +1,9 @@
 package com.automl.web.controller;
 
-import com.automl.api.service.DatasetOperatorService;
-import com.google.common.collect.Maps;
-import io.github.qingguox.json.JacksonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author wangqingwei
@@ -18,28 +13,4 @@ import java.util.Map;
 @RequestMapping("/namespaces/{namespace}/datasets")
 public class DatasetController {
     private static final Logger logger = LoggerFactory.getLogger(DatasetController.class);
-
-    @Autowired
-    private DatasetOperatorService datasetOperatorService;
-
-    @PostMapping(value = "/")
-    public Object putDataset(@PathVariable(value = "namespace") long namespaceId,
-        @RequestBody Map<String, String> map) {
-        logger
-            .info("putDataset req namespaceId : {}, map : {}",
-                namespaceId, JacksonUtils.toJSON(map));
-        final String name = map.get("name");
-        final String description = map.get("description");
-        final String uri = map.get("uri");
-//        return datasetOperatorService.insertDataset(namespaceId, name, description, uri);
-        return new Object();
-    }
-
-    @GetMapping(value = "/")
-    public Object getDatasetList(@PathVariable(value = "namespace") long namespaceId,
-        @RequestParam(value = "offset", required = false, defaultValue = "0") int offset,
-        @RequestParam(value = "limit", required = false, defaultValue = "100") int limit) {
-//        return datasetOperatorService.getByNamespaceAndLimit(namespaceId, offset, limit);
-        return Maps.newHashMap();
-    }
 }
